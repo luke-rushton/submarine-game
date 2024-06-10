061e9b4b0f7da802525535a84ece4850f37d5094
luke rushton <lukerushton@uvic.ca>
2024-06-09 22:18:04 -0700

    added start button functionality

--- game/game.js
+++ game/game.js
@@ -1,6 +1,34 @@
+//build the canvas 
 const canvas = document.getElementById("game-canvas");
 const context = canvas.getContext("2d");
-
 canvas.width = 1000;
 canvas.height = 500;
-console.log('test');
+
+class Game {
+    constructor(){
+        this.hello = "hello world!";
+        this.canvas = canvas; //maybe initialize inside object?
+    }
+    start(){
+        console.log(this.hello);
+    }
+}
+
+
+
+
+
+//hide ui elements and start game
+const startScreenElem = document.getElementById("start-screen");
+document.addEventListener("keydown", (event) => {
+    if(event.key === ' '){
+        startScreenElem.style.display = 'none';
+        startGame();
+    }
+});
+
+function startGame(){ //initializes the game object
+    const game = new Game();
+    game.start();
+
+}
