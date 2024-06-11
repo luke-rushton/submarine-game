a7fc15291dd2dc196ed77a1de605107774100176
Luke Rushton <luke.rushtonx@gmail.com>
2024-06-11 13:55:34 -0700

    added rock object with draw and move functionality + added animation frame

--- game/game.js
+++ game/game.js
@@ -4,13 +4,44 @@ const context = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 500;
 
+class Rock {
+    constructor(x = 950, y = 0) { //update or remove default values
+        this.x = x;
+        this.y = y;
+        this.color = '#000000';
+    }
+
+    draw() {
+        context.beginPath();
+        context.rect(this.x, this.y, 50, 50);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    move() {
+        this.x -= 5; //update this to change scroll speed
+        if (this.x < -50) {
+            this.x = canvas.width + 50;
+        }
+    }
+}
+
 class Game {
-    constructor(){
+    constructor() {
         this.hello = "hello world!";
         this.canvas = canvas; //maybe initialize inside object?
+        this.traverseSpeed = 25; //change for faster/slower game scrolling
     }
-    start(){
-        console.log(this.hello);
+    start() {
+        const rock = new Rock();
+        rock.draw();
+        const animate = () => { //split animate into animation function?
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            rock.move();
+            rock.draw();
+            requestAnimationFrame(animate);
+        };
+        animate();
     }
 }
 
@@ -21,13 +52,13 @@ class Game {
 //hide ui elements and start game
 const startScreenElem = document.getElementById("start-screen");
 document.addEventListener("keydown", (event) => {
-    if(event.key === ' '){
+    if (event.key === ' ') {
         startScreenElem.style.display = 'none';
         startGame();
     }
 });
 
-function startGame(){ //initializes the game object
+function startGame() { //initializes the game object
     const game = new Game();
     game.start();
 
