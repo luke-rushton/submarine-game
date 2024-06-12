74f7e21297a60bf91049ed436122955fe42081b0
luke rushton <lukerushton@uvic.ca>
2024-06-11 18:58:29 -0700

    file partitioning

--- game/game.js
+++ game/game.js
@@ -6,61 +6,8 @@ canvas.height = 500;
 const submarineImg = new Image();
 submarineImg.src = "sub-basic.png";
 
-class Submarine {
-    constructor() {
-        this.x = 250; //update initial position here there is a minor float up at start due to this position. update to start at neutral?
-        this.y = 250;
-        this.ceiling = 0; //these 2 are for submarine up and down max height
-        this.floor = 450;
-        this.neutral = (this.ceiling + this.floor) / 2; //take into account sub sprite offset?
-        this.movingUp = false;
-        this.movingDown = false;
-        this.neutralFloat = submarineImg; //maybe directly assign src
-    }
-    draw() {
-        context.drawImage(this.neutralFloat, this.x, this.y);
-    }
-    float() { //maybe combine float and sink?
-        if (this.y > this.ceiling) {
-            this.y -= 5;
-        }
-    }
-    sink() {
-        if (this.y < this.floor) {
-            this.y += 5;
-        }
-    }
-    reset() {
-        if (this.y < this.neutral) {
-            this.y += 2.5; //floats back at half speed of control
-        }
-        if (this.y > this.neutral) {
-            this.y -= 2.5;
-        }
-    }
-}
-
-class Rock { //might want to change name?
-    constructor(x = 950, y = 0) { //update or remove default values
-        this.x = x;
-        this.y = y;
-        this.color = '#000000';
-    }
-
-    draw() {
-        context.beginPath();
-        context.rect(this.x, this.y, 50, 50);
-        context.fillStyle = this.color;
-        context.fill();
-    }
-
-    move() {
-        this.x -= 5; //update this to change scroll speed
-        if (this.x < -50) {
-            this.x = canvas.width + 50;
-        }
-    }
-}
+//stops spamming start game
+let gameStarted = false;
 
 class Game {
     constructor() {
@@ -127,8 +74,11 @@ class Game {
 const startScreenElem = document.getElementById("start-screen");
 document.addEventListener("keydown", (event) => {
     if (event.key === ' ') {
-        startScreenElem.style.display = 'none';
-        startGame();
+        if(!gameStarted){
+            gameStarted = true;
+            startScreenElem.style.display = 'none';
+            startGame();
+        }
     }
 });
 
